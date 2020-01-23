f781e7c039690f7c3a12b186454c91b304e0ce31
Matthieu Labas <matthieu.labas@deepgray.fr>
2020-01-23 11:18:42 +0100

    Configurable tmp directory for petalinux patch.

--- resources/patch-petalinux-installer.sh
+++ resources/patch-petalinux-installer.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 PLXI="$1"
+TMP="${2:-/tmp}"
 [ ! -f "$PLXI" ] && echo "Cannot find $PLXI" && exit 1
-SZLEFT=$(df -B 1 /tmp | tail -1 | awk '{ print $4 }')
+SZLEFT=$(df -B 1 "$TMP" | tail -1 | awk '{ print $4 }')
 SZFILE=$(stat -c %s "$PLXI")
-[ $SZLEFT -lt $SZFILE ] && echo "Not enough space on /tmp" && exit 2
+[ $SZLEFT -lt $SZFILE ] && echo "Not enough space on $TMP" && exit 2
 SKIP=$(awk '/^##__PLNXSDK_FOLLOWS__/ { print NR + 1; exit 0; }' "$PLXI")
 # TODO: Locally check checksum and patch checksum check
-head -$(($SKIP -1)) "$PLXI" | sed 's/^accept_license$/# accept_license/' > /tmp/plsh \
-&& tail -n +$SKIP "$PLXI" > /tmp/plbin \
-&& cat /tmp/plsh /tmp/plbin > "$(basename $PLXI)" \
-&& rm -f /tmp/plsh /tmp/plbin
+head -$(($SKIP -1)) "$PLXI" | sed 's/^accept_license$/# accept_license/' > "$TMP/plsh" \
+&& tail -n +$SKIP "$PLXI" > "$TMP/plbin" \
+&& cat "$TMP/plsh" "$TMP/plbin" > "$(basename $PLXI)" \
+&& rm -f "$TMP/plsh" "$TMP/plbin"
