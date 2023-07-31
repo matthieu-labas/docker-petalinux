fafaca393913c753e4ae5dc356fab3a46e0da700
Matthieu Labas <matthieu.labas@deepgray.fr>
2023-07-31 21:23:30 +0200

    Cleaning.

--- resources/package_SDK.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SDK=${1:-SDK}
-VERFILE="$SDK/data/dynamic_language_bundle.properties"
-[ ! -f "$VERFILE" ] && echo "Could not locate version file" && exit 1
-eval $(grep ^DEFAULT_DESTINATION_FOLDER_LIN= "$VERFILE")
-[ -z "$DEFAULT_DESTINATION_FOLDER_LIN" ] && echo "Could not guess version" && exit 2
-XILVER=$(basename "$DEFAULT_DESTINATION_FOLDER_LIN")
-cd "$SDK" && tar czf ../Xilinx-SDK-v${XILVER}.tgz * && cd .. && rm -rf "$SDK"
